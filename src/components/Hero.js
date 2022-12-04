c9680e5805c8465b9cc14f64fb0022a4b6179c07
Imon <contact@imonweb.co.uk>
2022-12-04 00:09:18 +0000

    completed

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -73,15 +73,15 @@ const Hero = () => {
       </button>
       </header>
       {/* text & image wrapper */}
-      <div>
+      <div className='flex flex-col items-center lg:flex-row min-h-[740px]'>
         {/* text */}
-        <div>
+        <div className='flex-1  flex flex-col justify-center items-start'>
           <h1 className='text-5xl text-primary font-bold mb-6 leading-[60px] dark:text-white'>
             Online Accounting <br /> <span class="text-accent">Fast & Uncomplicated</span>
           </h1>
           <p className='font-light text-grey mb-12 max-w-[597px] dark:text-white'>We specialize in small businesss. Our goal is to eliminate bureaucracy, crating a modern relationship between your company and the accountant. </p>
           {/* checked items */}
-          <div>
+          <div className='flex flex-col gap-y-6 mb-12'>
                {/* item */}
                <div className='flex items-center gap-x-2'>
                   {/* item icon */}
@@ -89,13 +89,24 @@ const Hero = () => {
                     <BsCheck />
                   </div>
                   {/* item text */}
-                  <p className='text-base dark:text-white'>Have you accounting 100% online.</p>
+                  <p className='text-base font-light dark:text-white'>Have your accounting 100% online.</p>
+               </div>
+               {/* item */}
+               <div className='flex items-center gap-x-2'>
+                  {/* item icon */}
+                  <div className='bg-accent/10 text-accent w-[20px] h-[20px] rounded-full flex justify-center items-center dark:bg-accent/70 dark:text-white'>
+                    <BsCheck />
+                  </div>
+                  {/* item text */}
+                  <p className='text-base font-light dark:text-white'>Save with plans starting at £10/month.</p>
                </div>
           </div> {/* checked items end  */}
+          {/* button */}
+          <button className='btn'>Discover our plans</button>
               
         </div>
         {/* image */}
-        <div className='hidden lg:flex'>
+        <div className='hidden lg:flex self-end'>
           <img src={GirlImg} alt="" />
         </div>
       </div>
